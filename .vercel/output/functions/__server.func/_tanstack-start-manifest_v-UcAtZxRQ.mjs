e14ea34dfb000abf0fc1bb3d3e13f8071e57112a
jayandisemini2003-pixel <jayandisemini2003@gmail.com>
2026-07-26 21:51:02 +0530

    fix: remove stale committed .vercel/output folder from git tracking

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-UcAtZxRQ.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Jayandi/Downloads/semini  protfolio/semini  protfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DI5GDvWq.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DI5GDvWq.js" } }] }, "/": { filePath: "C:/Users/Jayandi/Downloads/semini  protfolio/semini  protfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-gHmFynrw.js"] } } });
-export {
-  tsrStartManifest
-};
